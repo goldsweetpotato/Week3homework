142aa145f0cf0697fe71c9379218308bb94126c8
goldsweetpotato <56987753+goldsweetpotato@users.noreply.github.com>
2024-10-01 23:19:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,23 @@ import os
 st.title("Week 3 homework")
 
 prompt = st.text_input("Share with us your experience of the latest trip")
+
+### Load your API Key
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAIKEY"]
+
+### OpenAI stuff: Creative: High temperature
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": prompt}
+  ],
+  temperature = 1.8,
+  max_tokens=20
+)
+
+### Display
+st.write(
+    "Creative: " + response.choices[0].message.content
+)
